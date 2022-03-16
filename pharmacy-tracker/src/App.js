42e13c82c777ad980f4fb7bac6a390b0678e3352
Cristina <cristina.falcon109@alu.ulpgc.es>
2022-03-16 18:21:18 +0000

    **

---
+++ pharmacy-tracker/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import  { PharmaciesList } from './pharmacies/PharmaciesList';
+import { dataProvider } from './firebase/firebaseConfig.js';
+
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+
+
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="pharmacies" list={PharmaciesList} />
+    </Admin>
+);
+
+export default App;
