d3d72371d7ed950a16350640caac1c2b47b7a1e5
fr0ziii <fr0ziisc2@gmail.com>
2022-03-21 10:27:55 +0000

    first commit

--- pharmacy-tracker/src/App.js
+++ pharmacy-tracker/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import  { PharmaciesList } from './pharmacies/PharmaciesList';
 import { dataProvider } from './firebase/firebaseConfig.js';
+import { Login } from './login/Login';
 
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
@@ -9,6 +10,7 @@ import { Admin, Resource } from 'react-admin';
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="pharmacies" list={PharmaciesList} />
+
     </Admin>
 );
 
