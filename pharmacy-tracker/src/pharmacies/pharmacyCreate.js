aa594c54c1b60276953daedd6b9e853dfd541e38
Ayoze <AyozeMN@hotmail.com>
2022-03-21 16:28:37 +0000

    time pickers added

--- pharmacy-tracker/src/pharmacies/pharmacyCreate.js
+++ pharmacy-tracker/src/pharmacies/pharmacyCreate.js
@@ -7,6 +7,28 @@ const CreateTitle = () => {
     return <span>Create pharmacy</span>;
 };
 
+const validateLatField = myLat => {
+    const latRegEx = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)/;
+    if(latRegEx.test(myLat)) {
+
+    }
+};
+
+const validateLngField = myLng => {
+    const lngRegEx = /\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
+    if(lngRegEx.test(myLng)) {
+
+    }
+};
+
+export const LatLngInput = () => (
+    <span>
+        <NumberInput source="Localización.latitude" label="Latitude" />
+        &nbsp;
+        <NumberInput source="Localización.longitude" label="Longitude" />
+    </span>
+);
+
 export const PharmacyCreateView = (props) => {
     return (
         <Create {...props}
@@ -14,13 +36,12 @@ export const PharmacyCreateView = (props) => {
             <SimpleForm>
                 <TextInput source="Direcci n" label="Address" validate={[required()]}/>
                 <TextInput source="Telefono" label="Phone" validate={[required()]}/>
-                <TextInput source="Apertura Matutina" label="Morning Opening" validate={[required()]}/>
-                <TextInput source="Cierre Matutino" label="Morning Closing" validate={[required()]}/>
-                <TextInput source="Apertura Vespertina" label="Evening Opening" validate={[required()]}/>
-                <TextInput source="Cierre Vespertino" label="Evening Closing" validate={[required()]}/>
+                <TextInput type="time" source="Apertura Matutina" label="Morning Opening" validate={[required()]}/>
+                <TextInput type="time" source="Cierre Matutino" label="Morning Closing" validate={[required()]}/>
+                <TextInput type="time" source="Apertura Vespertina" label="Evening Opening" validate={[required()]}/>
+                <TextInput type="time" source="Cierre Vespertino" label="Evening Closing" validate={[required()]}/>
                 <TextInput source="Municipio" label="City" validate={[required()]}/>
-                <NumberInput source="Localización.latitude" label="Latitude" validate={[required()]}/>
-                <NumberInput source="Localización.longitude" label="Longitude" validate={[required()]}/>
+                <LatLngInput validate={[required()]} />
             </SimpleForm>
         </Create>
     );
--- pharmacy-tracker/src/pharmacies/pharmacyEdit.js
+++ pharmacy-tracker/src/pharmacies/pharmacyEdit.js
@@ -16,10 +16,10 @@ export const PharmacyEditView = (props) => {
             <SimpleForm>
                 <TextInput source="Direcci n" label="Address" validate={[required()]}/>
                 <TextInput source="Telefono" label="Phone" />
-                <TextInput source="Apertura Matutina" label="Morning Opening" />
-                <TextInput source="Cierre Matutino" label="Morning Closing" />
-                <TextInput source="Apertura Vespertina" label="Evening Opening" />
-                <TextInput source="Cierre Vespertino" label="Evening Closing" />
+                <TextInput type="time" source="Apertura Matutina" label="Morning Opening" />
+                <TextInput type="time" source="Cierre Matutino" label="Morning Closing" />
+                <TextInput type="time" source="Apertura Vespertina" label="Evening Opening" />
+                <TextInput type="time" source="Cierre Vespertino" label="Evening Closing" />
                 <TextInput source="Municipio" label="City" />
                 <TextInput source="Comentarios" label="Coordinates" />
             </SimpleForm>
