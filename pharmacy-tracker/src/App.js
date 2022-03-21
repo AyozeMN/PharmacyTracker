2983185c33b65f9072c9f2672218e19554ea6afe
Ayoze <AyozeMN@hotmail.com>
2022-03-21 16:33:41 +0000

    Edit view added

--- pharmacy-tracker/src/App.js
+++ pharmacy-tracker/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { dataProvider } from './firebase/firebaseConfig.js';
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
+import { PharmacyListView } from './pharmacies/pharmacyList';
+import { PharmacyCreateView } from './pharmacies/pharmacyCreate';
+import { PharmacyEditView } from './pharmacies/pharmacyEdit';
 
-
-const App = () => (
+const App = () => (    
     <Admin dataProvider={dataProvider}>
-        <Resource name="pharmacies" list={PharmaciesList} />
+        <Resource name="pharmacies" create={PharmacyCreateView} edit={PharmacyEditView} list={PharmacyListView} />
     </Admin>
 );
 
