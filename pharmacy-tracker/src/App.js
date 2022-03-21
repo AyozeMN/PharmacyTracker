35323cf31c28f69581323171b9aa69bd7493eb87
fr0ziii <fr0ziisc2@gmail.com>
2022-03-21 10:18:25 +0000

    Revert "David: US. Login and Logout"
    This reverts commit f1cdce2a30031f8c86b13006dfdd2d8488024041.

--- pharmacy-tracker/src/App.js
+++ pharmacy-tracker/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import  { PharmaciesList } from './pharmacies/PharmaciesList';
-import { Login } from './login/login'
 import { dataProvider } from './firebase/firebaseConfig.js';
 
 import * as React from "react";
@@ -10,7 +9,6 @@ import { Admin, Resource } from 'react-admin';
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="pharmacies" list={PharmaciesList} />
-        <Resource name="login" icon={Login}></Resource>
     </Admin>
 );
 
