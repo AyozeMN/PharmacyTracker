f1cdce2a30031f8c86b13006dfdd2d8488024041
fr0ziii <fr0ziisc2@gmail.com>
2022-03-21 10:17:34 +0000

    David: US. Login and Logout

--- pharmacy-tracker/src/App.js
+++ pharmacy-tracker/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import  { PharmaciesList } from './pharmacies/PharmaciesList';
+import { Login } from './login/login'
 import { dataProvider } from './firebase/firebaseConfig.js';
 
 import * as React from "react";
@@ -9,6 +10,7 @@ import { Admin, Resource } from 'react-admin';
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="pharmacies" list={PharmaciesList} />
+        <Resource name="login" icon={Login}></Resource>
     </Admin>
 );
 
